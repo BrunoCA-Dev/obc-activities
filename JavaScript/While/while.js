9af061ed9fdd460e10d3f143831281b2ec9158e9
Bruno Araujo <bruno.araujo4454@gmail.com>
2023-10-17 10:44:18 -0300

    While structure example

---
+++ JavaScript/While/while.js
@@ -0,0 +1,9 @@
+let car = 80
+
+while (car>=1) {
+    alert ("The car are running at " + car + " Km/Hr " + "but its slowing down")
+    car -=20
+    alert ("Slowing down 20 Km/Hr ....")
+}
+
+alert ("The car stopped")
