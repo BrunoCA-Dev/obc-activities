5e7a5e777051c9cb1f492d653a2f6757bf21a5f6
Bruno Araujo <bruno.araujo4454@gmail.com>
2023-10-17 15:10:44 -0300

    It runs a menu that let you choose between some options and it keeps looping while you dont hit the Exit option

---
+++ JavaScript/Interactive Menu/interactiveMenu.js
@@ -0,0 +1,20 @@
+
+do {
+    menu = prompt("Choose an option:\n-Gorilla\n-Dog\n-Cat\n-Exit")
+        switch (menu) {
+            case "Gorilla":
+                alert ("Uh uh uh ah aH AH")
+                break
+            case "Dog":
+                alert ("Woof woof")
+                break
+            case "Cat":
+                alert ("Meow!")
+                break
+            case "Exit":
+                break
+        default:
+            alert ("Wrong option, dont forget to use Upper case")
+}} while (menu!=="Exit")
+
+alert ("Okay, bye bye")
