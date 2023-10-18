f24b583937bd4a50982be2a5a0492e652a7b4f28
Bruno Araujo <bruno.araujo4454@gmail.com>
2023-10-18 18:31:06 -0300

    Fixed some bugs on a for loop

--- JavaScript/For/for.js
+++ JavaScript/For/for.js
@@ -1,5 +1,5 @@
 let fstName = "Bruno Araujo"
 
-for (let i = 0; i<fstName.length; i++) {
+for (let i = fstName.length -1; i>=0; i--) {
     console.log(fstName[i])
 }
---
+++ JavaScript/Palindrome/palindrome.js
@@ -0,0 +1,5 @@
+let input = prompt("Type a word and the program will process and check if its a palindrome")
+
+for (let i = input.lenght; i<fstName.length; i--) {
+    console.log(input[i])
+}
