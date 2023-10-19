c902dddb06858f69191845b055bd1b12f7d56957
Bruno Araujo <bruno.araujo4454@gmail.com>
2023-10-19 15:27:06 -0300

    Program that check if a word is a palindrome or not

--- JavaScript/For/for.js
+++ JavaScript/For/for.js
@@ -3,3 +3,7 @@ let fstName = "Bruno Araujo"
 for (let i = fstName.length -1; i>=0; i--) {
     console.log(fstName[i])
 }
+
+for (let i = 0; i>=fstName.length; i++) {
+    console.log(fstName[i])
+}
--- JavaScript/Palindrome/palindrome.js
+++ JavaScript/Palindrome/palindrome.js
@@ -1,5 +1,19 @@
-let input = prompt("Type a word and the program will process and check if its a palindrome")
+let input = prompt("Type a word and the program will process and check if its a palindrome (palindrome is a word that are the same no matter if you read it from left to the right or right to the left")
 
-for (let i = input.lenght; i<fstName.length; i--) {
-    console.log(input[i])
+let inputOne = ""
+let inputTwo = ""
+
+for (let i = 0; i<input.length; i++) {
+    inputOne += (input[i])
 }
+
+for (let i = input.length -1; i>=0; i--) {
+    inputTwo += (input[i])
+}
+
+if (inputOne===inputTwo)
+ alert("You are a palindrome beast")
+else 
+  alert("You are not a palindrome beast, the word that you inserted cannot be read from right to left, here's why:\n" +
+"Left to right: " + inputOne + 
+"\nRight to left: " + inputTwo )
