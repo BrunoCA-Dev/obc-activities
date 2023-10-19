9c858e842b723a9a7bbb99211dacc282b7ed7dc5
Bruno Araujo <bruno.araujo4454@gmail.com>
2023-10-19 17:32:46 -0300

    Data structure first activity - Arrays

---
+++ JavaScript/Data Structure/Arrays/array.js
@@ -0,0 +1,10 @@
+const shoppingList = []
+console.log(shoppingList)
+
+shoppingList [0] = "Rice"
+shoppingList [1] = "Shampoo"
+shoppingList [2] = "Beans"
+shoppingList [3] = "Soy Sauce"
+shoppingList [4] = "Fish"
+
+alert ("You got a " + shoppingList[3])
---
+++ JavaScript/Data Structure/Arrays2/arrays2.js
@@ -0,0 +1,8 @@
+const arr = ["Frodo", "Sam", "Merry", "Merry", "Pippin"]
+
+console.log(arr)
+
+// add elements
+
+arr.push("Boromir")
+console.log(arr)
