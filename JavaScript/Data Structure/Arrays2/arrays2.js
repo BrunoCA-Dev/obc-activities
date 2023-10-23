c0aa52a27b36912d9a63ec039959579f065e6956
Bruno Araujo <bruno.araujo4454@gmail.com>
2023-10-23 19:04:01 -0300

    Data structure #2

--- JavaScript/Data Structure/Arrays2/arrays2.js
+++ JavaScript/Data Structure/Arrays2/arrays2.js
@@ -1,8 +1,25 @@
-const arr = ["Frodo", "Sam", "Merry", "Merry", "Pippin"]
+const arr = ["Gandalf", "Frodo", "Sam", "Merry", "Merry", "Pippin"]
 
 console.log(arr)
 
-// add elements
-
+// PUSH - ADD elements at the END of the array
 arr.push("Boromir")
 console.log(arr)
+
+// UNSHIFT -  ADD an element at the START of the array
+arr.unshift("Borromir-Unshift")
+console.log(arr)
+
+// POP - REMOVE an element at the END of the array
+let lastElement = arr.pop()
+console.log(arr)
+console.log(lastElement)
+
+// SHIFT - REMOVE an element at the START of the array
+lastElement = arr.shift()
+console.log(arr)
+console.log(lastElement)
+
+// INCLUDES - SEARCH for an element on the array
+const include = arr.includes("Gandalf")
+console.log(include)
