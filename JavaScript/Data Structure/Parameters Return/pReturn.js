cff219a5cf6d817f42061b65858e8ba558858b20
Bruno Araujo <bruno.araujo4454@gmail.com>
2023-11-07 18:03:22 -0300

    Functions and functions return activity

---
+++ JavaScript/Data Structure/Parameters Return/pReturn.js
@@ -0,0 +1,66 @@
+function averageCalculator (a, b) {
+    const average = (a + b) / 2
+    return average // return the value and store it on an variable
+}
+
+const result = averageCalculator(10,10)
+console.log (result)
+
+function createProduct(name,price) {
+    const product = {
+        name,
+        price,
+        stock: 1
+    }
+    return product
+}
+
+const notebook = createProduct("Dell", 6996)
+
+console.log(notebook)
+
+//////////////////////////////////////////////////
+
+//function retangularArea(base, hight){
+//    return base * hight // Return direct on the console
+//}
+
+//console.log(retangularArea(10,10))
+
+//////////////////////////////////////////////////
+
+function retangularArea(base, hight){
+       return base * hight
+    }
+
+function squareArea (side){
+    return retangularArea(side,side) // A function can call another function on its index
+}
+
+console.log(retangularArea (5,5))
+
+console.log(squareArea(9))
+
+///////////////////////////////////////////////////
+
+function hello(){
+    let text = "..."
+    return text // return always finish the function, leaving everything after the return unusable
+    text = "Hello, World!"
+    console.log(text)
+}
+
+console.log(hello(hello))
+
+/////////////////////////////////////////////////
+
+function old(age){
+    if(age>=18) {
+        return "You are 18 years old or older"
+    } else {
+        return "You are not older than 18"
+    }
+
+}
+
+console.log(old(21))
---
+++ JavaScript/Data Structure/Parameters/parameters.js
@@ -0,0 +1,54 @@
+function double (x = 0){
+    alert ("The double of " + x + " is " + (x*2))
+}
+
+//double(5)
+
+function sayHello (name = "horrible thing") {
+    alert ("Hello, " + name + " !")
+}
+
+//sayHello ()
+
+function addition (a,b) {
+    alert ("The result of the addition is " + (a+b))
+}
+
+//addition (50,50)
+
+function createUser (name, email, password, type) {
+    const user = {
+        name: name,
+        email: email,
+        password: password,
+        type: type
+    }
+
+    console.log(user)
+}
+
+//createUser ("bruno", "bruno.araujo4454@gmail.com", "123456", "admin")
+
+function newUser (name, type ="admin", email, password) { // The order of parameters mattersm default parameters should allways be at the end
+    const user = {
+        name: name,
+        email: email,
+        password: password,
+        type: type
+    }
+console.log(user)
+}
+
+newUser ("bruno", "bruno.araujo4454@gmail.com", "123456")
+
+const userData = {
+    name:"Yasuo",
+    password:"123456",
+    email:"cataclism@paradis.com",
+    type:"default"
+}
+
+function objectParamater (userData) {
+    console.log(userData)
+}
+
