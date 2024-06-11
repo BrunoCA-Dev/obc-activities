1de3fd51a5778a365d0d6157a786e15258189923
Bruno Araujo <bruno.araujo4454@gmail.com>
2024-06-11 17:58:35 -0300

    Patch 06.11.2024

--- JavaScript/Data Structure/Projects/Final Project/final.js
+++ JavaScript/Data Structure/Projects/Final Project/final.js
@@ -14,7 +14,7 @@ function jobList (){
 
 }
 
-//Function that creates a new job position as an object and adds it to the jobPosition array's of belford roxo cacts
+//Function that creates a new job position as an object and adds it to the jobPosition array's of belford roxo cacts's
 function jobNew (){
     let newJob = {}
     newJob.name = prompt("Job name")
