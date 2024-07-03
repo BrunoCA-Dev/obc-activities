3d06f2bac282527c0cada45f805b7fb7d84f16a8
Bruno Araujo <bruno.araujo4454@gmail.com>
2024-07-03 15:06:16 -0300

    patch 07.03.2024

--- JavaScript/Data Structure/Activities/Reduce Filter Map Sort/arrayHof.js
+++ JavaScript/Data Structure/Activities/Reduce Filter Map Sort/arrayHof.js
@@ -53,7 +53,7 @@ const races = characters.reduce(function(acumulativeValue, character) {
 
 console.log(races)
 
-// Sorties
+// Sortie's
 
 characters.sort(function(a,b) {
     return a.level - b.level 
